1b465e25edd14abb20dc8d2267a393bcd292b736
JosePucMoo <pucjl07@gmail.com>
2025-07-28 22:59:59 -0600

    Email

--- SistemaInventario/obj/Debug/net8.0/SistemaInventario.AssemblyInfo.cs
+++ SistemaInventario/obj/Debug/net8.0/SistemaInventario.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e9a0ee65893a4386b3707a5a381ded44416f5d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac88fbb2f6b59aaf15ae15b42e803214020a68c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
